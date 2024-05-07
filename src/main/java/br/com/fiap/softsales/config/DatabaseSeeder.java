252c24f73f70e0d62741e0ffbc296a41a2e071ee
Cassiyu <sakaicassio@gmail.com>
2024-05-07 16:26:02 -0300

    Corrigindo Erros

--- src/main/java/br/com/fiap/softsales/config/DatabaseSeeder.java
+++ src/main/java/br/com/fiap/softsales/config/DatabaseSeeder.java
@@ -27,10 +27,11 @@ public class DatabaseSeeder implements CommandLineRunner {
 
         produtoRepository.saveAll(
                 List.of(
-                        Produto.builder().id(1L).nome("Produto A").valor(new BigDecimal("10")).build(),
-                        Produto.builder().id(2L).nome("Produto B").valor(new BigDecimal("20")).build(),
-                        Produto.builder().id(3L).nome("Produto C").valor(new BigDecimal("30")).build(),
-                        Produto.builder().id(4L).nome("Produto D").valor(new BigDecimal("40")).build()));
+                        Produto.builder().id(1L).nome("Produto A").valor(new BigDecimal(10      )).build(),
+                        Produto.builder().id(2L).nome("Produto B").valor(new BigDecimal(20)).build(),
+                        Produto.builder().id(3L).nome("Produto C").valor(new BigDecimal(30)).build(),
+                        Produto.builder().id(4L).nome("Produto D").valor(new BigDecimal(40)).build(),
+                        Produto.builder().id(5L).nome("Produto E").valor(new BigDecimal(50)).build()));
 
         vendaRepository.saveAll(
                 List.of(
